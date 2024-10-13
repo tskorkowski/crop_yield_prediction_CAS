03b1a8441f98632ff9174242444042d5130af39f
tomasz <t.skorkowski@gmail.com>
2024-10-13 15:49:37 +0000

    hist-per-img

--- 2_gc-pipeline/utils/constants.py
+++ 2_gc-pipeline/utils/constants.py
@@ -23,6 +23,6 @@ HIST_BINS_LIST = [np.linspace(1, 2500, 33),
                   np.linspace(100, 5500, 33),
                   np.linspace(100, 5500, 33),
                   np.linspace(1,1000,33),
-                  np.linspace(1,20,20),
+                  np.linspace(1,20,33),
                   np.linspace(100, 5500, 33),
                   np.linspace(100, 5500, 33),]
